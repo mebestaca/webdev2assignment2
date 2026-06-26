04b9027010f507ca08128e49712c20b41c750384
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-26 17:08:36 -0600

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,13 @@
 'use client'
-import { useState } from "react";
 import StudentListSection from "./components/StudentListSection";
-import NavBar from "./components/NavBar";
-import RegistrationForm from "./components/RegistrationForm";
-import Footer from "./components/Footer";
-import { students as initialStudents } from "@/lib/data";
-import { RegistrationData } from "./lib/schema";
 
 export default function Home() {
-  const [students, setStudents] = useState(initialStudents);
 
-  const addStudent = (newStudent: RegistrationData) => {
-    setStudents([
-      ...students, 
-      {
-        ...newStudent, 
-        dateOfBirth: new Date(newStudent.dateOfBirth),
-        id: crypto.randomUUID(),
-      },
-    ]);
-  };
 
   return (
     <div>
       <div id="body" className="bg-slate-300 p-10 flex-1 ">
-        <StudentListSection students={students} />
+        <StudentListSection />
       </div>
     </div>
   );
