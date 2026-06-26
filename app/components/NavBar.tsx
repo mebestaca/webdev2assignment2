c932b466d60a67145f00faa27f9dd039a104b211
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-06-26 12:50:43 -0600

    updated alignment of header

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <nav className="bg-slate-700">
       <header className="bg-white border-b border-gray-200 px-6 py-4">
-        <div className="max-w-5xl mx-auto flex items-center justify-between">
+        <div className="max-w-5xl flex items-center">
           <div className="text-lg font-bold text-gray-900">
             <h1 className="text-lg font-bold text-gray-900">Assignment 2</h1>
             <p className="text-xs text-gray-400">CPRG 306 Web Development 2</p>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,25 +1,25 @@
-"use client"
-import React from 'react'
-import { RegistrationData, registrationSchema } from '../lib/schema';
-import {useForm} from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
+// "use client"
+// import React from 'react'
+// import { RegistrationData, registrationSchema } from '../lib/schema';
+// import {useForm} from 'react-hook-form';
+// import { zodResolver } from '@hookform/resolvers/zod';
 
-const RegistrationPage = () => {
-  const {register, handleSubmit, trigger, formState:{errors,isSubmitting, isSubmitSuccessful} } = 
-    useForm<RegistrationData>({
-        resolver:zodResolver(registrationSchema),
-        defaultValues:{
-          firstName: "",
-          lasttName:"",
-          dateOfBirth:"",
-          grade:"",
-        }
-    })
-  return (
-    <div>
-      <h2>Registration</h2>
-    </div>
-  )
-}
+// const RegistrationPage = () => {
+//   const {register, handleSubmit, trigger, formState:{errors,isSubmitting, isSubmitSuccessful} } = 
+//     useForm<RegistrationData>({
+//         resolver:zodResolver(registrationSchema),
+//         defaultValues:{
+//           firstName: "",
+//           lasttName:"",
+//           dateOfBirth:"",
+//           grade:"",
+//         }
+//     })
+//   return (
+//     <div>
+//       <h2>Registration</h2>
+//     </div>
+//   )
+// }
 
-export default RegistrationPage
+// export default RegistrationPage
