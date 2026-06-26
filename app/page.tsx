a2a7743023191e083936519dd4590ce2647cc08a
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-06-26 15:53:34 -0600

    updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,23 @@
 'use client'
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
 import StudentListSection from "./components/StudentListSection";
-import NavBar from "./components/NavBar";
-import RegistrationForm from "./components/RegistrationForm";
-import Footer from "./components/Footer";
-import { students as initialStudents } from "@/lib/data";
-import { RegistrationData } from "./lib/schema";
 
 export default function Home() {
-  const [students, setStudents] = useState(initialStudents);
+  const [students, setStudents] = useState([])
 
-  const addStudent = (newStudent: RegistrationData) => {
-    setStudents([
-      ...students, 
-      {
-        ...newStudent, 
-        dateOfBirth: new Date(newStudent.dateOfBirth),
-        id: crypto.randomUUID(),
-      },
-    ]);
-  };
+  useEffect(() => {
+    const savedStudents = localStorage.getItem("students");
+  
+    if (savedStudents) {
+      setStudents(JSON.parse(savedStudents));
+    }
+  
+  }, []);
 
   return (
-    <div>
-      <div id="body" className="bg-slate-300 p-10 flex-1 ">
-        <StudentListSection students={students} />
-      </div>
-    </div>
+    <main className="min-h-screen bg-slate-100 p-10">
+      <StudentListSection students={students} />
+    </main>
   );
 }
