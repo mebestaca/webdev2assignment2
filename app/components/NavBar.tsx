713edf0b58b20e63554ce4736f0d4301d713230f
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-26 09:40:11 -0600

    links added

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <header className='bg-white border-b border-gray-200 px-6 py-4'>
-      <div className='max-w-5xl mx-auto flex items-center justify-between'>
-        <div className='text-lg font-bold text-gray-900'>
-            <h1 className='text-lg font-bold text-gray-900'>List of Students</h1>
-            <p className='text-xs text-gray-400'>CPRG 306 Web Development 2</p>
+    <nav>
+      <header className='bg-white border-b border-gray-200 px-6 py-4'>
+        <div className='max-w-5xl mx-auto flex items-center justify-between'>
+          <div className='text-lg font-bold text-gray-900'>
+              <h1 className='text-lg font-bold text-gray-900'>List of Students</h1>
+              <p className='text-xs text-gray-400'>CPRG 306 Web Development 2</p>
+          </div>
         </div>
+      </header>
+      <div>
+        <Link href="/">
+          Students List
+        </Link>
+        <Link href="/register">
+          Add a Student
+        </Link>
       </div>
-    </header>
+    </nav>
   )
 }
 
---
+++ app/components/RegisterStudent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegisterStudent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default RegisterStudent
