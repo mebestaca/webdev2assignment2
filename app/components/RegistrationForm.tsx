1eb8eee89216755e23502595d5fc47fe4deb64d8
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-26 15:12:47 -0600

    useform changed to form

--- app/components/RegistrationForm.tsx
+++ app/components/RegistrationForm.tsx
@@ -18,7 +18,7 @@ const RegistrationForm = ({form} : RegistrationProps) => {
             errors,
             isSubmitting,
             isSubmitSuccessful
-          }} = useForm<RegistrationData>();
+          }} = form;
 
   const inputClassStyle = 'w-full rounded-lg border border-slate-300 px-3 py-2 text-sm text-slate-900' +
         'focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent';
