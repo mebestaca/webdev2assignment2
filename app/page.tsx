c5cce716b8a59228bff62b8c2be387a9378a7f1f
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-06-26 17:20:39 -0600

    clean homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@
 import StudentListSection from "./components/StudentListSection";
 
 export default function Home() {
-
-
   return (
     <div>
       <div id="body" className="bg-slate-300 p-10 flex-1 ">
