754d816b6bd4704e88aa60c15c137f719f44d05d
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-06-26 12:42:29 -0600

    updated student data

--- lib/data.ts
+++ lib/data.ts
@@ -11,35 +11,35 @@ export const students : Student[] = [
         id: "A001",
         firstName: "Marc Edison",
         lastName: "Estaca",
-        dateOfBirth: new Date("2008-01-01"),
+        dateOfBirth: new Date("1994-04-15"),
         grade: "A+"
     },
     {
         id: "A002",
         firstName: "Reiner Justin",
         lastName: "Realica",
-        dateOfBirth: new Date("2008-01-02"),
+        dateOfBirth: new Date("1998-03-28"),
         grade: "A+"
     },
     {
         id: "A003",
         firstName: "Joshua",
         lastName: "Dyck",
-        dateOfBirth: new Date("2008-01-03"),
+        dateOfBirth: new Date("2003-01-03"),
         grade: "A+"
     },
     {
         id: "A004",
         firstName: "John",
         lastName: "Endfield",
-        dateOfBirth: new Date("2008-01-04"),
+        dateOfBirth: new Date("2000-06-26"),
         grade: "A+"
     },
     {
         id: "A005",
         firstName: "Jane",
         lastName: "Hypergryph",
-        dateOfBirth: new Date("2008-01-05"),
+        dateOfBirth: new Date("1995-08-15"),
         grade: "A+"
     }
 ]
