91a6c4d80e05010cbf016193167f8ff3bc4c2c36
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-06-26 12:58:44 -0600

    adjusted spacing of cards

--- app/components/StudentCard.tsx
+++ app/components/StudentCard.tsx
@@ -8,7 +8,7 @@ type StudentProp = {
 
 const StudentCard = ({student} : StudentProp) => {
   return (
-    <div className="bg-white border border-slate-200 rounded-xl p-5 shadow-sm-flex flex-col gap-10">
+    <div className="bg-white border border-slate-200 rounded-xl p-5 shadow-sm-flex flex-col gap-10 mb-3">
       <h2 className="text-base font-bold text-slate-800">{`${student.firstName} ${student.lastName}`}</h2>
       <p className="text-sm text-slate-500 flex-1">{student.dateOfBirth.toLocaleDateString('en-US')}</p>
       <div className="flex items-center justify-between mt-1">
