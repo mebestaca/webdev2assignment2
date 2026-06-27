b8131b4658e308927c32da86fdfacb7736ac1d93
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-26 18:02:29 -0600

    added documentation

--- app/shared/StudentContext.ts
+++ app/shared/StudentContext.ts
@@ -1,3 +1,13 @@
+/**
+ * @author  Marc Edison Estaca, 
+ *          Reiner Justin Realica, 
+ *          Joshua Dyck
+ * @version 1.0
+ * @description Contains the structure of the context. students is a list
+ *          of Student objects, and addStudent is a method that accepts
+ *          a Student object. Mainly used in the context provider
+ */
+
 import { Student } from "@/lib/data";
 import { createContext } from "react";
 
