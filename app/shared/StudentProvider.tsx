554cb975cfd9ef5ccf93ee6b7c8db37fa1faf158
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-26 18:00:33 -0600

    added documentation

--- app/shared/StudentProvider.tsx
+++ app/shared/StudentProvider.tsx
@@ -1,12 +1,22 @@
+/**
+ * @author  Marc Edison Estaca, 
+ *          Reiner Justin Realica, 
+ *          Joshua Dyck
+ * @version 1.0
+ * @returns StudentContext
+ * @description Context so that the useState is available everywhere that this
+ *          component is wrapped around with. Features students hook which 
+ *          provides a list of students, and setStudents, a hook that allows
+ *          the program to add a new student object to the list
+ */
+
 "use client";
 
 import { useState } from "react";
 import { StudentContext } from "./StudentContext";
 import { Student, students as studentsList} from "@/lib/data";
 
-export default function StudentProvider({ children}: {
-  children: React.ReactNode;
-}) {
+export default function StudentProvider({ children}: {children: React.ReactNode;}) {
   const [students, setStudents] = useState<Student[]>(studentsList);
 
   const addStudent = (student: Student) => {
